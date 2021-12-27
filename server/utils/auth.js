427ec50b9441204690a27b322cffe57f1bd0efb2
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-27 11:04:23 -0500

    added auth file for login and json web token

---
+++ server/utils/auth.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+
+const secret = Process.env.Secret;
+console.log(secret);
+const expiration = "2h";
+
+module.exports = {
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
