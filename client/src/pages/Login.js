eba0149d646ad787f078780083dd53aec70afab1
Kai <mordechaigerstley@gmail.com>
2021-12-29 12:16:20 -0500

    made login html

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,9 +2,39 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <form action="action_page.php" method="post">
+      <div class="container">
+        <label for="uname">
+          <b>Username</b>
+        </label>
+        <input type="text" placeholder="Enter Username" name="uname" required />
+
+        <label for="psw">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          required
+        />
+
+        <button type="submit">Login</button>
+        <label>
+          <input type="checkbox" checked="checked" name="remember" /> Remember
+          me
+        </label>
+      </div>
+
+      <div class="container" style="background-color: #f1f1f1">
+        <button type="button" class="cancelbtn">
+          Cancel
+        </button>
+        <span class="psw">
+          Forgot <a href="#">password?</a>
+        </span>
+      </div>
+    </form>
   );
 };
 
