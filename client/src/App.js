79a55e1c04224101461e292004837b0f34655e5e
Teddy Sahr <tedward1218@gmail.com>
2021-12-29 19:52:56 -0500

    imported apolloclient, inmemorycache, and apolloprovider from apollo/client and set up apolloclient and apolloprovider in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Search from "./pages/Search";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div>
-        <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/search" element={<Search />} />
-        </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/profile" element={<Profile />} />
+            <Route exact path="/search" element={<Search />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 };
 
