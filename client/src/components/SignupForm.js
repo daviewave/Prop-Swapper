11167621569c6146025a108024031bae5efc1494
Teddy Sahr <tedward1218@gmail.com>
2022-01-02 15:55:47 -0500

    created validation for signup form and imported new file with validation helper functions

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,33 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "@mui/material/Container";
-import { TextField } from "@mui/material";
+// Here we import a helper function that will check if the email is valid
+import { checkPassword, validateEmail } from "../utils/helpers";
 
 const SignupForm = () => {
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "userName") {
+      setUserName(inputValue);
+    } else {
+      setPassword(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage("Email or username is invalid");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    if (!checkPassword(password)) {
+      setErrorMessage(
+        `Choose a more secure password for the account: ${userName}`
+      );
+      return;
+    }
+    alert(`Hello ${userName}`);
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+
+    //THIS IS WHERE THE MUTATION WILL GO
+    setUserName("");
+    setPassword("");
+    setEmail("");
+  };
   return (
     <Container id="form-container" maxWidth="xs">
-      <h1>SIGN UP FORM!</h1>
-      <TextField
-        required
-        id="standard-required"
-        label="Required"
-        defaultValue="Username"
-        variant="standard"
-      />
-      <TextField
-        required
-        id="standard-required"
-        label="Required"
-        defaultValue="Email"
-        variant="standard"
-      />
-      <TextField
-        id="standard-password-input"
-        label="Password"
-        type="password"
-        variant="standard"
-      />
-
-      <button>Submit</button>
+      <div>
+        <p>Hello {userName}</p>
+        <form className="form">
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+          />
+          <input
+            value={userName}
+            name="userName"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="username"
+          />
+          <input
+            value={password}
+            name="password"
+            onChange={handleInputChange}
+            type="password"
+            placeholder="Password"
+          />
+          <button type="button" onClick={handleFormSubmit}>
+            Submit
+          </button>
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+      </div>
     </Container>
   );
 };
