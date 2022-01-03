3a8edeab5b148d1483c1f52a910935719f028bc8
Teddy Sahr <tedward1218@gmail.com>
2022-01-03 11:57:17 -0500

    added simple dropdown form to search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container } from "@mui/material";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <Container>
+      <div>
+        <h1>Start searching for your next swap!</h1>
+        <form className="form">
+          <select name="states">
+            <option value="NY">NY</option>
+            <option value="FL">FL</option>
+            <option value="IL">IL</option>
+            <option value="TN">TN</option>
+          </select>
+        </form>
+      </div>
+    </Container>
+  );
 };
 
 export default Search;
