87de843c581aa100ff78709cf28fba58013410b6
Teddy Sahr <tedward1218@gmail.com>
2022-01-03 13:55:11 -0500

    added logo

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Container } from "@mui/material";
-import { Box } from "@mui/system";
+
 import { Link } from "react-router-dom";
 
 const Footer = () => {
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -5,7 +5,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav id="navbar">
-      <h1>PROP SWAP</h1>
+      <img
+        className="image"
+        src="client/public/Screen_Shot_2022-01-03_at_1.25.26_PM-removebg-preview.png"
+        alt="logo"
+      />
       <ul id="navtabs">
         <Link style={{ textDecoration: "none" }} to="/profile">
           <li>My Prop</li>
