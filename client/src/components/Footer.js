b42b2050c0248a6f5eefa1aa38cd4b5431aa4460
Teddy Sahr <tedward1218@gmail.com>
2022-01-03 13:07:21 -0500

    added an about page and linked it in the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { Container } from "@mui/material";
 import { Box } from "@mui/system";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer id="footer">
-      <Container maxWidth="lg">
-        <Box>
-          <h1>Footer</h1>
-        </Box>
-      </Container>
+      <Link to="/">About</Link>
     </footer>
   );
 };
