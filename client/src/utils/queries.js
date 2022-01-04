50640d03103f2c1888b891936915a9ef10fc2d02
Teddy Sahr <tedward1218@gmail.com>
2022-01-04 12:08:46 -0500

    added users query to queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -17,6 +17,21 @@ export const QUERY_USER = gql`
   }
 `;
 
+export const QUERY_USERS = gql`
+  query users {
+    users {
+      username
+      email
+      property {
+        address
+        state
+        city
+        bedrooms
+      }
+    }
+  }
+`;
+
 export const QUERY_ME = gql`
   query me {
     me {
